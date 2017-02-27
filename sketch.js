186d9b4b36946022ab4c81910a39fd07bec4ee5b
Nero <penance316@gmail.com>
2017-02-27 13:19:20 +0000

    refactor a bit more. Some vars to lets and some class methods

--- sketch.js
+++ sketch.js
@@ -23,7 +23,7 @@ window.setup = function () {
     textSize( symbolSize );
 };
 
-window.draw = function() {
+window.draw = function () {
     background( 0, 150 );
     streams.forEach( function ( stream ) {
         stream.render();
@@ -35,59 +35,63 @@ class Symbol {
         this.x = x;
         this.y = y;
         this.speed = speed;
-        this.switchInterval = round( random( 2, 25 ) );
         this.first = first;
         this.value = '';
-
-        this.setToRandomSymbol = function () {
-            //p5 frameCount value
-            if ( frameCount % this.switchInterval == 0 ) {
-                this.value = String.fromCharCode(
-                    0x30A0 + round( random( 0, 96 ) )
-                );
-            }
-        };
-
-        this.rain = function () {
-            this.y = (this.y >= height) ? 0 : this.y += this.speed;
-        }
     }
+
+    switchInterval() {
+        return round( random( 2, 25 ) )
+    };
+
+    setToRandomSymbol() {
+        //p5 frameCount value
+        if ( frameCount % this.switchInterval() == 0 ) {
+            this.value = String.fromCharCode(
+                0x30A0 + round( random( 0, 96 ) )
+            );
+        }
+    };
+
+    rain() {
+        this.y = (this.y >= height) ? 0 : this.y += this.speed
+    };
+
 }
 
 class Stream {
 
     constructor() {
-
         this.symbols = [];
         this.totalSymbols = round( random( 5, 30 ) );
         this.speed = random( 5, 20 );
+    };
 
-        this.generateSymbols = function ( x, y ) {
-            var first = round( random( 0, 4 ) ) == true;
-            for ( var i = 0; i < this.totalSymbols; i++ ) {
-                let symbol = new Symbol( x, y, this.speed, first );
-                symbol.setToRandomSymbol();
-                this.symbols.push( symbol );
-                // put next symbol above the previous
-                y -= symbolSize;
-                first = false;
-            }
-        };
-
-        this.render = function () {
-            this.symbols.forEach( function ( symbol ) {
-                // make first symbol glow
-                if ( symbol.first ) {
-                    fill( 140, 255, 200 );
-                } else {
-                    fill( 0, 255, 70 );
-                }
-
-                text( symbol.value, symbol.x, symbol.y );
-                symbol.rain();
-                symbol.setToRandomSymbol();
-            } );
+    generateSymbols( x, y ) {
+        let first = round( random( 0, 4 ) ) == true;
+        for ( let i = 0; i < this.totalSymbols; i++ ) {
+            let symbol = new Symbol( x, y, this.speed, first );
+            symbol.setToRandomSymbol();
+            this.symbols.push( symbol );
+            // put next symbol above the previous
+            y -= symbolSize;
+            first = false;
         }
+    };
+
+    render() {
+        this.symbols.forEach( function ( symbol ) {
+            // make first symbol glow
+            if ( symbol.first ) {
+                fill( 140, 255, 200 );
+            } else {
+                fill( 0, 255, 70 );
+            }
+
+            text( symbol.value, symbol.x, symbol.y );
+            symbol.rain();
+            symbol.setToRandomSymbol();
+        } );
     }
+
 }
 
